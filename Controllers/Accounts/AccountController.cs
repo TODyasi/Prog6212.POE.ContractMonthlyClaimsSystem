2ec70dabe7f10791cb0436468455b6223463d8d9
TODyasi <okuhledyasi7@gmail.com>
2024-10-09 08:34:07 +0000

    8/09/2024

--- Controllers/Accounts/AccountController.cs
+++ Controllers/Accounts/AccountController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CMCS.Infrastructure.Data;
+using CMCS.Domain.Entities.UserModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Prog6212.POE.ContractMonthlyClaimsSystem.Controllers.Accounts
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        //access the database for users
+        private readonly ApplicationDbContext _usersDb;
+
+        public AccountController(ApplicationDbContext usersDb)
+        {
+            _usersDb = usersDb;
+        }
+        //registration page
+        public IActionResult Register()
         {
             return View();
+        }
+
+        //login page
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserModel user)
+        {
+            //Add user to the database
+          _usersDb.Users.Add(user);
+
+            //Save changes to the database
+            await _usersDb.SaveChangesAsync();
+
+            //redirect to login page
+            return RedirectToAction("Login");
+
         }
     }
 }
